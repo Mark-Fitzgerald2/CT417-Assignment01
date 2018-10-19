60daded8ca788a87b150b594ad61cf82657f9d74
I342039 <I342039@DUBN34198882A.emea.global.corp.sap>
2018-10-19 18:18:43 +0100

    Added comments

--- src/test/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/studentRegTest.java
+++ src/test/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/studentRegTest.java
@@ -23,11 +23,14 @@ public class studentRegTest {
     
     @Test
      public void StudentReg_Test() {
+        //create a course object
         Course ece = new Course("Electronic and Computer Engineering (ECE)", DateTime.parse("10/09/2018", DateTimeFormat.forPattern("dd/MM/yyyy")), DateTime.parse("10/05/2019", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        //uses getters to check basic details of course
         assertEquals(ece.getName(), "Electronic and Computer Engineering (ECE)");
         assertEquals(ece.getStartDate(),DateTime.parse("10/09/2018", DateTimeFormat.forPattern("dd/MM/yyyy")));
         assertEquals(ece.getEndDate(),DateTime.parse("10/05/2019", DateTimeFormat.forPattern("dd/MM/yyyy")));
         
+        //create modules and check getters
         Module softEng = new Module("Software Engineering III", "CT417");
         assertEquals(softEng.getName(), "Software Engineering III");
         assertEquals(softEng.getId(), "CT417");
@@ -52,6 +55,7 @@ public class studentRegTest {
         assertEquals(soc.getName(), "System on Chip Design I");
         assertEquals(soc.getId(), "EE451");
         
+        //add all the modules to course
         ece.addModule(softEng);
         ece.addModule(ml);
         ece.addModule(fyp);
@@ -59,9 +63,11 @@ public class studentRegTest {
         ece.addModule(dsp);
         ece.addModule(soc);
         
+        //create an array list of courses
         ArrayList<Course> courses = new ArrayList<Course>();
         courses.add(ece);
         
+        //create an arraylist of modules
         ArrayList<Module> eceModules = new ArrayList<Module>();
         eceModules.add(softEng);
         eceModules.add(ml);
@@ -70,12 +76,15 @@ public class studentRegTest {
         eceModules.add(dsp);
         eceModules.add(soc);
         
+        //check if getModules returns all the correct modules
         assertEquals(ece.getModules(), eceModules);
+        //check if modules return all courses
         assertEquals(fyp.getCourses(), courses);
         assertEquals(tele.getCourses(), courses);
         assertEquals(dsp.getCourses(), courses);
         assertEquals(soc.getCourses(), courses);
         
+        //create students
         Student student1 = new Student("Paddy Smith", 21, DateTime.parse("25/05/1997", DateTimeFormat.forPattern("dd/MM/yyyy")), "154789");
         Student student2 = new Student("Joe Lo Trgulio", 20, DateTime.parse("17/12/1997", DateTimeFormat.forPattern("dd/MM/yyyy")), "132654");
         Student student3 = new Student("Andy Samberg", 25, DateTime.parse("12/01/1993", DateTimeFormat.forPattern("dd/MM/yyyy")), "198745");
@@ -87,27 +96,33 @@ public class studentRegTest {
         Student student9 = new Student("Stephanie Beatriz", 20, DateTime.parse("01/01/1998", DateTimeFormat.forPattern("dd/MM/yyyy")), "785435");
         Student student10 = new Student("Raymond Holt", 23, DateTime.parse("18/07/1995", DateTimeFormat.forPattern("dd/MM/yyyy")), "455164");
         
+        //check all the basic getters of the students
         assertEquals(student1.getName(), "Paddy Smith");
         assertEquals(student1.getAge(), 21);
         assertEquals(student1.getDOB(), DateTime.parse("25/05/1997", DateTimeFormat.forPattern("dd/MM/yyyy")));
         assertEquals(student1.getId(), "154789");
         assertEquals(student1.getUsername(), "Paddy Smith 21");
         
+        //register students to ECE
         ece.addStudent(student6);
         ece.addStudent(student7);
         ece.addStudent(student8);
         ece.addStudent(student9);
         ece.addStudent(student10);
         
+        //create a second course
         Course csit = new Course("Computer Science and Information Technology (CS&IT)", DateTime.parse("10/09/2018", DateTimeFormat.forPattern("dd/MM/yyyy")), DateTime.parse("10/05/2019", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        //register half the students to csit
         csit.addStudent(student1);
         csit.addStudent(student2);
         csit.addStudent(student3);
         csit.addStudent(student4);
         csit.addStudent(student5);
+        //add two modules to csit
         csit.addModule(softEng);
         csit.addModule(ml);
         
+        //create an arraylist of csit students
         ArrayList<Student> csitStudents = new ArrayList<Student>();
         csitStudents.add(student1);
         csitStudents.add(student2);
@@ -115,6 +130,7 @@ public class studentRegTest {
         csitStudents.add(student4);
         csitStudents.add(student5);
         
+        //create an array list of ece students
         ArrayList<Student> eceStudents = new ArrayList<Student>();
         eceStudents.add(student6);
         eceStudents.add(student7);
@@ -122,24 +138,33 @@ public class studentRegTest {
         eceStudents.add(student9);
         eceStudents.add(student10);
         
+        //create an arraylist of courses
         ArrayList<Course> st1Course = new ArrayList<Course>();
         st1Course.add(csit);
         
+        //create an array list of modules
         ArrayList<Module> st1Module = new ArrayList<Module>();
         st1Module.add(softEng);
         st1Module.add(ml);
         
+        //check if csit has all 5 students
         assertEquals(csit.getStudents(), csitStudents);
+        //check if student has the correct courses and modules
         assertEquals(student1.getCourses(), st1Course);
         assertEquals(student1.getModules(), st1Module);
         
+        //add csit to courses
         courses.add(csit);
+        //check if softEng and ml are assigned to both ece and csit
         assertEquals(softEng.getCourses(), courses);
         assertEquals(ml.getCourses(), courses);
         
+        //create an array list of all students
         ArrayList<Student> allStudents = new ArrayList<Student>();
+        //add ece students and csit students to this array
         allStudents.addAll(eceStudents);
         allStudents.addAll(csitStudents);
+        //check if all modules return the correct students
         assertEquals(ml.getStudents(), allStudents);
         assertEquals(softEng.getStudents(), allStudents);
         assertEquals(fyp.getStudents(), eceStudents);
