acf64a85c495d20fc43eaa235b8e6c7cfc2d593b
I342039 <I342039@DUBN34198882A.emea.global.corp.sap>
2018-10-19 13:48:07 +0100

    Added Getters and Setters for Name and Id in Module

--- src/main/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/Module.java
+++ src/main/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/Module.java
@@ -12,8 +12,26 @@ package ie.nuigalway.fitzgeraldmark.ct417assignment1;
 public class Module {
     private String name;
     private String id;
+    
     public Module(String name, String id){
         this.name = name;
         this.id = id;
     }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
+
