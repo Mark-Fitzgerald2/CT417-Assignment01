ca5f01fa7289eb342f8fccc658c9d901dd21db2e
I342039 <I342039@DUBN34198882A.emea.global.corp.sap>
2018-10-19 16:53:01 +0100

    Created JUnit test

---
+++ src/test/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/studentRegTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.nuigalway.fitzgeraldmark.ct417assignment1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author I342039
+ */
+public class studentRegTest {
+    
+    public studentRegTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+     public void StudentReg_Test() {
+        
+     }
+}
